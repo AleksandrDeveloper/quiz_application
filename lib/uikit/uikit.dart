137e9833a554ab67c107cf742ddfce81eac6a895
AleksandrDeveloper <aleksandr.developer.web@gmail.com>
2022-09-18 17:14:15 +0300

    refactoring name file and widgets

--- lib/uikit/uikit.dart
+++ lib/uikit/uikit.dart
@@ -2,3 +2,4 @@ export 'quiz_app_button.dart';
 export 'quiz_app_bar.dart';
 export 'theme.dart';
 export 'colors.dart';
+export 'quiz_app_text_field_dropdown.dart';
--- lib/widgets/welcome_screen_widgets/form_container_welcome.dart
+++ lib/widgets/welcome_screen_widgets/form_container_welcome.dart
@@ -1,4 +1,4 @@
-import 'package:dropdown_textfield/dropdown_textfield.dart';
+ import 'package:dropdown_textfield/dropdown_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quiz_application/bloc/welcome/welcome_bloc.dart';
@@ -138,41 +138,4 @@ class _FormContainerWelcomeState extends State<FormContainerWelcome> {
   }
 }
 
-class QuizAppTextFieldDropdown extends StatelessWidget {
-  final String? title;
-  final SingleValueDropDownController controller;
-  final List<DropDownValueModel>? listValue;
-  final int? listValueLength;
 
-  const QuizAppTextFieldDropdown({
-    Key? key,
-    this.title,
-    this.listValueLength,
-    required SingleValueDropDownController controller,
-    required this.listValue,
-  })  : controller = controller,
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return DropDownTextField(
-      singleController: controller,
-      clearOption: false,
-      dropDownItemCount: listValueLength ?? 1,
-      dropDownList: listValue ?? [],
-      textFieldDecoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(50),
-          borderSide: BorderSide(
-            width: 3,
-            color: AppColor.background,
-          ),
-        ),
-        labelStyle: theme.textTheme.bodyText1,
-        labelText: title,
-      ),
-    );
-  }
-}
